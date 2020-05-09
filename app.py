981550dc4405db9b77a1ef1cf90a090585764596
ferrakkem <ferrakkem@gmail.com>
2020-05-09 02:56:43 -0400

    initial commit of Sum in Loop problem of codeabbey

--- app.py
+++ app.py
@@ -3,7 +3,6 @@
 def sum_in_loop():
     nun_data = int(input("How how many number you wants to add: "))
     my_li = list(map(int, input('Enter you number by space: ').split()))[:nun_data]
-    print(my_li)
     sum = 0
 
     for i in my_li:
